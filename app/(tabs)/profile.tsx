9a4ee58e2649bc92dfa6bd44fd6daa6e9b34c785
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-08-29 20:18:08 +0530

    add default to saved and profile tabs

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, Image } from 'react-native';
+import React from 'react';
+import { icons } from '@/constants/icons';
+
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col'>
+        <Image 
+          source={ icons.person }
+          className='size-10'
+          tintColor={ '#fff' }
+        />
+        <Text className='text-gray-500 text-base mt-2'>Profile</Text>
+      </View>
     </View>
   )
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, Image } from 'react-native';
+import React from 'react';
+import { icons } from '@/constants/icons';
+
 
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col'>
+        <Image 
+          source={ icons.save }
+          className='size-10'
+          tintColor={ '#fff' }
+        />
+        <Text className='text-gray-500 text-base mt-2'>Saved</Text>
+      </View>
     </View>
   )
 }
